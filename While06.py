35e6b05439697100877cf6b091ccd0cb7903fa60
PRESTIGE <mamatqulov@gmail.com>
2023-01-08 22:47:20 +0500

    o6

--- While06.py
+++ While06.py
@@ -11,7 +11,7 @@ def main(s):
     idx = 0
     sum = 0
     while leng != 0:
-        if s[idx] != 'a' and s[idx] != 'e' and s[idx] != 'i' and s[idx] != 'o' and s[idx] != 'u':
+        if s[idx] == 'a' or s[idx] == 'e' or s[idx] == 'i' or s[idx] == 'o' or s[idx] == 'u':
             sum += 1
 
         idx += 1
