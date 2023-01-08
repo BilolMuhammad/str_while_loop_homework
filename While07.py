224565893b02cb725bc9386cd0f0a8f17096a0e2
PRESTIGE <mamatqulov@gmail.com>
2023-01-08 22:24:48 +0500

    o7

--- While07.py
+++ While07.py
@@ -6,4 +6,15 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    leng = len(s)
+    idx = 0
+    sum = 0
+    while leng != 0:
+        if int(s[idx]) % 2 == 0:
+            sum += 1
+        idx += 1
+        leng -= 1
+    return sum
+
+
+print(main('231468'))
